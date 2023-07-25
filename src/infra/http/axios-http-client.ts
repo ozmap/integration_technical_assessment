736cb8ce1c25efd84e7247f7d5437b482c07a5aa
Gabriel Nascimento <gabrielnascimento.grn@gmail.com>
2023-07-25 18:20:31 -0300

    feat: ensure AxiosHttpClient returns correct response

--- src/infra/http/axios-http-client.ts
+++ src/infra/http/axios-http-client.ts
@@ -1,10 +1,13 @@
-import axios from 'axios';
+import axios, { type AxiosResponse } from 'axios';
 import { type HttpClient } from '../../data/interfaces';
 import { type HttpRequest, type HttpResponse } from '../../data/types';
 
 export class AxiosHttpClient implements HttpClient {
-  async request (request: HttpRequest): Promise<HttpResponse<any>> {
-    await axios.request(request);
-    return null;
+  async request (request: HttpRequest): Promise<HttpResponse> {
+    const axiosResponse: AxiosResponse = await axios.request(request);
+    return {
+      statusCode: axiosResponse.status,
+      body: axiosResponse.data
+    };
   }
 }
