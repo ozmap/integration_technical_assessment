b5d549fdc02d1d27fac1c82e44f93a9940d39174
Gabriel Nascimento <gabrielnascimento.grn@gmail.com>
2023-07-25 14:36:20 -0300

    feat: ensure RemoteGetUser calls Logger.info() with correct value

--- src/data/usecases/remote-get-user/remote-get-user.ts
+++ src/data/usecases/remote-get-user/remote-get-user.ts
@@ -1,18 +1,20 @@
 import { UnexpectedError } from '../../../domain/errors/unexpected-error';
 import { type UserModel } from '../../../domain/models';
 import { type GetUser } from '../../../domain/usecases';
-import { type HttpClient } from '../../interfaces';
+import { type Logger, type HttpClient } from '../../interfaces';
 import { HttpStatusCode, type HttpRequest } from '../../types';
 
 export class RemoteGetUser implements GetUser {
   constructor (
     private readonly url: string,
-    private readonly httpClient: HttpClient
+    private readonly httpClient: HttpClient,
+    private readonly logger: Logger
   ) {
     this.httpClient = httpClient;
   }
 
   async get (): Promise<UserModel> {
+    await this.logger.info({ message: 'Retrieving new user information' });
     const request: HttpRequest = {
       method: 'get',
       url: this.url
