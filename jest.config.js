d0e28e3aa426d60c59d3ad8eb317430b0bfca78b
Gabriel Nascimento <gabrielnascimento.grn@gmail.com>
2023-07-24 21:56:53 -0300

    chore: add jest

---
+++ jest.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    roots: ['<rootDir>/src/'],
+    collectCoverageFrom: [
+      '<rootDir>/src/**/*.ts'
+    ],
+    coverageDirectory: 'coverage',
+    testEnvironment: 'jest-environment-node',
+    transform: {
+      '.+\\.ts': 'ts-jest'
+    }
+  };
+  
