bbcc4bebd2055d30401cb1119d518279cecd3bc3
Gabriel Nascimento <gabrielnascimento.grn@gmail.com>
2023-07-26 10:44:23 -0300

    refactor: move SUT creation to a factory method

--- src/infra/report/local-markdown-reporter.spec.ts
+++ src/infra/report/local-markdown-reporter.spec.ts
@@ -1,18 +1,31 @@
 import { type ReportEntry } from '../../data/types';
 import { LocalMarkdownReporter } from './local-markdown-reporter';
 
+type SutTypes = {
+  sut: LocalMarkdownReporter
+  privateEntries: ReportEntry[]
+};
+
+const makeSut = (): SutTypes => {
+  const sut = new LocalMarkdownReporter();
+  const privateEntries = ((sut as any).entries as ReportEntry[]);
+
+  return {
+    sut,
+    privateEntries
+  };
+};
+
 describe('LocalMarkdownReporter', () => {
   test('should instantiate with an empty array', () => {
-    const sut = new LocalMarkdownReporter();
-
-    const privateEntries = (sut as any).entries;
+    const { privateEntries } = makeSut();
 
     expect(privateEntries).toEqual([]);
   });
 
   test('should add new ReportEntry object to entries', () => {
-    const sut = new LocalMarkdownReporter();
-    const privateEntries = (sut as any).entries;
+    const { sut, privateEntries } = makeSut();
+
     const data: ReportEntry = {
       action: 'Unit test',
       status: 'sucesso'
