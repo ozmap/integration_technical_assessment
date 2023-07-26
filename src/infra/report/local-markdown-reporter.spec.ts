9ca02653c56a4a229641ee90307c9234b78dc674
Gabriel Nascimento <gabrielnascimento.grn@gmail.com>
2023-07-26 14:01:36 -0300

    test: ensure LocalMarkdownReporter generates report correctly

--- src/infra/report/local-markdown-reporter.spec.ts
+++ src/infra/report/local-markdown-reporter.spec.ts
@@ -1,6 +1,16 @@
 import { type ReportEntry } from '../../data/types';
 import { LocalMarkdownReporter } from './local-markdown-reporter';
 
+import fs from 'fs/promises';
+
+jest.mock('fs/promises', () => ({
+  writeFile: jest.fn(),
+  mkdir: jest.fn()
+}));
+
+jest.useFakeTimers({ now: new Date('2023-01-01T10:30') });
+const mockDateTime = '2023-01-01_10-30';
+
 type SutTypes = {
   sut: LocalMarkdownReporter
   privateEntries: ReportEntry[]
@@ -23,16 +33,101 @@ describe('LocalMarkdownReporter', () => {
     expect(privateEntries).toEqual([]);
   });
 
-  test('should add new ReportEntry object to entries', () => {
-    const { sut, privateEntries } = makeSut();
+  describe('append()', () => {
+    test('should add new ReportEntry object to entries', () => {
+      const { sut, privateEntries } = makeSut();
 
-    const data: ReportEntry = {
-      action: 'Unit test',
-      status: 'sucesso'
-    };
+      const data: ReportEntry = {
+        action: 'Unit test',
+        status: 'sucesso'
+      };
 
-    sut.append(data);
+      sut.append(data);
 
-    expect(privateEntries).toEqual([data]);
+      expect(privateEntries).toEqual([data]);
+    });
+  });
+  describe('generate()', () => {
+    it('should format the current date and time correctly', () => {
+      const { sut } = makeSut();
+      const formattedDate = ((sut as any).getCurrentDateTime() as string);
+
+      expect(formattedDate).toMatch(mockDateTime);
+    });
+
+    it('should generate a valid file name', () => {
+      const { sut } = makeSut();
+      const formattedDate = ((sut as any).getFileName(mockDateTime) as string);
+
+      expect(formattedDate).toBe(`relatorio_${mockDateTime}.md`);
+    });
+
+    it('should add the title with the correct datetime', () => {
+      const { sut } = makeSut();
+      ((sut as any).addTitle(mockDateTime));
+      const reportTitle = ((sut as any).content as string);
+
+      expect(reportTitle).toBe(`# Relatório de Execução - Data: ${mockDateTime}\n\n`);
+    });
+
+    it('should add content correctly', () => {
+      const { sut } = makeSut();
+
+      const entries: ReportEntry[] = [
+        {
+          action: 'action1',
+          status: 'erro',
+          message: 'Message for action1',
+          data: { key1: 'value1' }
+        },
+        {
+          action: 'action2',
+          status: 'erro',
+          message: 'Message for action2'
+        },
+        {
+          action: 'action3',
+          status: 'sucesso'
+        }
+      ];
+
+      entries.forEach((entry) => { sut.append(entry); });
+      ((sut as any).addContent(mockDateTime));
+      const content = ((sut as any).content as string);
+
+      const expectedContent =
+      '1. Ação: action1\n   ' +
+        '- Status: erro\n   ' +
+        '- Mensagem de Erro: Message for action1\n' +
+          '\n   - Dados:\n\n      ```json\n' +
+          '      {\n        "key1": "value1"\n      }\n' +
+          '      ```\n\n' +
+      '2. Ação: action2\n   ' +
+        '- Status: erro\n   ' +
+        '- Mensagem de Erro: Message for action2\n' +
+      '3. Ação: action3\n   ' +
+      '- Status: sucesso\n';
+
+      expect(content).toBe(expectedContent);
+    });
+
+    test('should create folder if not exists', async () => {
+      const { sut } = makeSut();
+      const mkdirSpy = jest.spyOn(fs, 'mkdir');
+
+      ((sut as any).getFolder());
+
+      expect(mkdirSpy).toHaveBeenCalledWith('reports');
+    });
+
+    test('should save report as markdown with correct path', async () => {
+      const { sut } = makeSut();
+      const writeFileSpy = jest.spyOn(fs, 'writeFile');
+
+      await sut.generate();
+      const reportContent = ((sut as any).content as string);
+
+      expect(writeFileSpy).toHaveBeenCalledWith(`reports/relatorio_${mockDateTime}.md`, reportContent);
+    });
   });
 });
