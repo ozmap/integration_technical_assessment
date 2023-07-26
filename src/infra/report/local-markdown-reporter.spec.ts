4a40e70393ba194c4084d0ecd5ab8b4ec4aa7235
Gabriel Nascimento <gabrielnascimento.grn@gmail.com>
2023-07-26 10:40:10 -0300

    test: ensure LocalMarkdownReporter adds new ReportEntry object to entries

--- src/infra/report/local-markdown-reporter.spec.ts
+++ src/infra/report/local-markdown-reporter.spec.ts
@@ -1,3 +1,4 @@
+import { type ReportEntry } from '../../data/types';
 import { LocalMarkdownReporter } from './local-markdown-reporter';
 
 describe('LocalMarkdownReporter', () => {
@@ -7,5 +8,18 @@ describe('LocalMarkdownReporter', () => {
     const privateEntries = (sut as any).entries;
 
     expect(privateEntries).toEqual([]);
+  });
+
+  test('should add new ReportEntry object to entries', () => {
+    const sut = new LocalMarkdownReporter();
+    const privateEntries = (sut as any).entries;
+    const data: ReportEntry = {
+      action: 'Unit test',
+      status: 'sucesso'
+    };
+
+    sut.append(data);
+
+    expect(privateEntries).toEqual([data]);
   });
 });
