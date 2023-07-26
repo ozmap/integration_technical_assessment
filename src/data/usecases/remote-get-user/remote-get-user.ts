40030779d35c1ca505d5733f3d9dfb516d8744e9
Gabriel Nascimento <gabrielnascimento.grn@gmail.com>
2023-07-26 15:29:50 -0300

    feat: ensure RemoteGetUser calls Reporter.append() with correct values

--- src/data/usecases/remote-get-user/remote-get-user.ts
+++ src/data/usecases/remote-get-user/remote-get-user.ts
@@ -2,13 +2,15 @@ import { UnexpectedError } from '../../../domain/errors/unexpected-error';
 import { type UserModel } from '../../../domain/models';
 import { type GetUser } from '../../../domain/usecases';
 import { type Logger, type HttpClient } from '../../interfaces';
-import { HttpStatusCode, type HttpRequest } from '../../types';
+import { type Reporter } from '../../interfaces/reporter';
+import { HttpStatusCode, type HttpRequest, type ReportEntry, ReportStatus } from '../../types';
 
 export class RemoteGetUser implements GetUser {
   constructor (
     private readonly url: string,
     private readonly httpClient: HttpClient,
-    private readonly logger: Logger
+    private readonly logger: Logger,
+    private readonly reporter: Reporter
   ) {
     this.httpClient = httpClient;
   }
@@ -42,8 +44,19 @@ export class RemoteGetUser implements GetUser {
     };
   };
 
+  private async handleLogAndReport ({ action, status, message, data }: ReportEntry): Promise<void> {
+    this.reporter.append({ action, status, message, data });
+    switch (status) {
+      case ReportStatus.error: { await this.logger.error({ message: action, error: data }); return; }
+      default: { await this.logger.info({ message: action, data }); }
+    }
+  }
+
   async get (): Promise<UserModel> {
-    await this.logger.info({ message: 'Retrieving new user information' });
+    await this.handleLogAndReport({
+      action: 'Retrieving new user information',
+      status: ReportStatus.pending
+    });
     const request: HttpRequest = {
       method: 'get',
       url: this.url
@@ -53,17 +66,20 @@ export class RemoteGetUser implements GetUser {
       case HttpStatusCode.ok: {
         const [userData] = httpResponse.body.results;
         const user = this.mapResponseToUserModel(userData);
-        await this.logger.info({
-          message: 'User information was successfully retrieved',
+        await this.handleLogAndReport({
+          action: 'User information was successfully retrieved',
+          status: ReportStatus.sucess,
           data: user
         });
         return user;
       }
       default: {
         const error = new UnexpectedError(httpResponse.body.error);
-        await this.logger.error({
-          message: 'An error occurred during user information retrieval',
-          error
+        await this.handleLogAndReport({
+          action: 'An error occurred during user information retrieval',
+          status: ReportStatus.error,
+          data: error,
+          message: error.message
         });
         throw error;
       }
